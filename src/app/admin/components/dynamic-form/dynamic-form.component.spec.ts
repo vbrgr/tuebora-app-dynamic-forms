1f552b537731e1dc3de57db083a088cc4f6f8841
govinda raju <raju.vallabhani@gmail.com>
2019-09-26 19:02:49 +0530

    unit testing

--- src/app/admin/components/dynamic-form/dynamic-form.component.spec.ts
+++ src/app/admin/components/dynamic-form/dynamic-form.component.spec.ts
@@ -1,19 +1,47 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { DynamicFormComponent } from './dynamic-form.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DebugElement } from '@angular/core';
 describe('DynamicFormComponent', () => {
   let component: DynamicFormComponent;
   let fixture: ComponentFixture<DynamicFormComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
+  let event: Event;
+  const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ DynamicFormComponent ],
-      imports: [ReactiveFormsModule, FormsModule],
+      imports: [ReactiveFormsModule, FormsModule, HttpClientTestingModule],
       schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
     })
-    .compileComponents();
+    .compileComponents().then(() => {
+      fixture = TestBed.createComponent(DynamicFormComponent);
+      component = fixture.componentInstance;
+      component.ngOnInit();
+      fixture.detectChanges();
+      de = fixture.debugElement.query(By.css('form'));
+      el = de.nativeElement;
+    });
   }));
+
+
+  /* it('form invalid when empty', () => {
+    Object.keys(component.form.controls).forEach(field => {
+      component.form.get(field).setValue('');
+    });
+    expect(component.form.valid).toBeFalsy();
+  }); */
+
+  it('form should be valid', () => {
+    Object.keys(component.form.controls).forEach(field => {
+    //  component.form.get(field).setValue('');
+    });
+    expect(component.form.valid).toBeTruthy();
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DynamicFormComponent);
